25a728a97a1f9c8f2c8c23d21525b1b8bc7cc28a
alehuo <aleksi.huotala@helsinki.fi>
2020-01-22 14:14:47 +0200

    Update

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyoidc_redis_session_backend",
+    name="pyoidc-redis-session-backend-alehuo",
     version="1.0.0",
-    author="Aleksi Huotala",
+    author="Aleksi H",
     author_email="aleksi.huotala@helsinki.fi",
     description="Redis-based session storage for pyoidc library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alehuo/pyoidc-redis-session-backend",
-    packages=setuptools.find_packages(),
+    packages=['pyoidc_redis_session_backend'],
+    py_modules=['pyoidc_redis_session_backend.RedisSessionBackend'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    license="MIT",
     install_requires=['oic', 'jsonpickle', 'redis', 'pycryptodome'],
     python_requires='>=3.6',
 )
