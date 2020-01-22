a60b8edd8a1a15fa7bbc2206e7b065dba1e7f520
alehuo <aleksi.huotala@helsinki.fi>
2020-01-22 12:50:02 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pyoidc-redis-session-backend
+
+## Developing
+
+1. Run `python3 -m venv env`
+2. Run `source env/bin/activate`
+3. Run `pip install -r requirements.txt`
--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['oic', 'jsonpickle', 'redis'],
+    install_requires=['oic', 'jsonpickle', 'redis', 'pycryptodome'],
     python_requires='>=3.6',
 )
