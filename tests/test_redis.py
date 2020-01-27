2bdf1f4cbc8ebe7737687522203b7e535c3401d5
alehuo <aleksi.huotala@helsinki.fi>
2020-01-27 10:53:05 +0200

    Split tests into multiple files

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,20 +1,7 @@
 from unittest import TestCase
-import jsonpickle
 import fakeredis
-from pyoidc_redis_session_backend import RSAKeyHandler, RedisSessionBackend
-from Cryptodome.PublicKey.RSA import RsaKey
+from pyoidc_redis_session_backend import RedisSessionBackend
 from Cryptodome.PublicKey import RSA
-
-class TestSerializer(TestCase):
-    def test_encode_works_1(self):
-        key = RSA.generate(2048)
-        encoded = jsonpickle.encode(key)
-
-    def test_decode_works_1(self):
-        key = RSA.generate(2048)
-        encoded_key = jsonpickle.encode(key)
-        decoded_key = jsonpickle.decode(encoded_key)
-        self.assertEqual(key, decoded_key)
 
 class TestRedis(TestCase):
     def test_set(self):
---
+++ tests/test_serializer.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import jsonpickle
+from pyoidc_redis_session_backend import RSAKeyHandler
+from Cryptodome.PublicKey.RSA import RsaKey
+from Cryptodome.PublicKey import RSA
+
+jsonpickle.register(RsaKey, RSAKeyHandler)
+
+class TestSerializer(TestCase):
+    def test_encode_works_1(self):
+        key = RSA.generate(2048)
+        encoded = jsonpickle.encode(key)
+
+    def test_decode_works_1(self):
+        key = RSA.generate(2048)
+        encoded_key = jsonpickle.encode(key)
+        decoded_key = jsonpickle.decode(encoded_key)
+        self.assertEqual(key, decoded_key)
